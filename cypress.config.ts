5b7ec17d2a4f27b41393ea864bc6f6bdc407db6c
Bohdan Hulobov <bogdan.gulobov@gmail.com>
2023-11-24 08:26:44 +0200

    cypress installation

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}";
+      baseUrl: "http://localhost:4200";
+    },
+  },
+});
