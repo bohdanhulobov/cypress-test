e280af216bc82ff37960df6323aa543832f0ad88
Bohdan Hulobov <89200310+bohdanhulobov@users.noreply.github.com>
2023-11-27 19:48:14 +0200

    Update cypress.config.ts
    Co-authored-by: Artem <shoiko.a.s@gmail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4200",
   },
 });
