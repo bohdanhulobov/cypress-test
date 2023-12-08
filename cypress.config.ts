7546dd55ccdce7fd3fee0ee87e16a5253651c6a5
Bohdan Hulobov <bogdan.gulobov@gmail.com>
2023-12-08 09:55:21 +0200

    lesson 17 homework

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportHeight: 800,
   viewportWidth: 1200,
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results/mochawesome-report",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {},
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
